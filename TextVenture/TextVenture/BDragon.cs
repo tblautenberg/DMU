adb03d18399a6819ed7d2be36a4df6bc58ced288
Tobias Blautenberg <tobbl01@eamv.dk>
2024-09-16 22:57:10 +0200

    Actually it was not added - but now it is :D

---
+++ TextVenture/TextVenture/BDragon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextVenture
+{
+    internal class BDragon
+    {
+        private string Name;
+        private int HP = 0;
+        private int Mana = 0;
+
+        public BDragon(string name, int hP, int mana)
+        {
+            Name = name;
+            HP = hP;
+            Mana = mana;
+        }
+
+        public List<String> DragonAbilities = new List<String>();
+
+        public void DragonClaw()
+        {
+            int RndNum = HelperClass.RndNumber();
+            Console.WriteLine($"The dragon uses a firce attack and deals {RndNum} damge to its foe!");
+            DragonAbilities.Add("DragonClaw");
+        }
+
+        public void FlameBreath()
+        {
+            Console.WriteLine($"The dragon unleash a firey breath on everything in front of him! It deals {HelperClass.RndNumber() + 15} damage!");
+            DragonAbilities.Add("FlameBreath");
+        }
+
+    }
+}
---
+++ TextVenture/TextVenture/Player.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextVenture
+{
+    internal class Player
+    {
+        private string Name;
+        private int Level;
+        private int HP;
+        private int Mana;
+        private int HealthPotAmount = 3;
+        private int ManaPotAmount = 3;
+
+        public Player(string name, int level, int hP, int mana)
+        {
+            Name = name;
+            Level = level;
+            HP = hP;
+            Mana = mana;
+        }
+
+        public void DrinkHealthPot()
+        {
+            if (HealthPotAmount > 0)
+            {
+                int RndNUm = HelperClass.RndNumber();
+                Console.WriteLine($"The player drinks a health potion and restores {RndNUm} health back!");
+                HealthPotAmount -= 1;
+            }
+            else
+            {
+                Console.WriteLine("The player is out of health pots!");
+            }
+        }
+
+        public void DrinkManaPot()
+        {
+            if (HealthPotAmount > 0)
+            {
+                int RndNUm = HelperClass.RndNumber();
+                Console.WriteLine($"The player drinks a mana potion and restores {RndNUm} Mana back!");
+                ManaPotAmount -= 1;
+            }
+            else
+            {
+                Console.WriteLine("The player is out of mana pots!");
+            }
+        }
+    }
+}

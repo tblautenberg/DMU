255a3adfe5911b198db9b4f6038b2127eb419111
Tobias Blautenberg <tobbl01@eamv.dk>
2024-09-26 22:48:42 +0200

    Added some more stuff to first ASP project

--- BirthdayInvitaionProgram/WebApplication4/WebApplication4/Controllers/HomeController.cs
+++ BirthdayInvitaionProgram/WebApplication4/WebApplication4/Controllers/HomeController.cs
@@ -1,29 +1,35 @@
 using Microsoft.AspNetCore.Mvc;
 using PartyInvites.Models;
-using System.Diagnostics;
-using WebApplication4.Models;
-
-namespace WebApplication4.Controllers
+namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         public IActionResult Index()
         {
             return View();
         }
-
         [HttpGet]
         public ViewResult RsvpForm()
         {
             return View();
         }
-
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            Repository.AddResponse(guestResponse);
-            return View("Thanks", guestResponse);
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
         }
     }
 }
--- BirthdayInvitaionProgram/WebApplication4/WebApplication4/obj/Release/net8.0/WebApplication4.AssemblyInfo.cs
+++ BirthdayInvitaionProgram/WebApplication4/WebApplication4/obj/Release/net8.0/WebApplication4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd078b7acb67ece8b1f93d229df5ec02dfdeedb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
